4106ad88de5a8e61d453bd9a8fe6a6976ac8706e
Vaishnavi <ghumarevaishnavib@gmail.com>
2024-07-14 22:05:52 +0530

    initial commit

---
+++ modules/init.js
@@ -0,0 +1,30 @@
+let { productData } = require("./data.js");
+const mongoose = require("mongoose");
+const Product=require("./product.js")
+const Post=require("./posts.js")
+const main = async () => {
+    await mongoose.connect('mongodb+srv://ghumarevaishnavib:2g92mEbVuSmtx9vG@cluster0.o4vvkzh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  }
+main().then(() => {
+    console.log("Connected....")
+}).catch((err) => {
+    console.log(err);
+})
+
+
+const initialize = async () => {
+    await Product.deleteMany({});
+    const data=await Product.insertMany(productData);
+//    initdata= initdata.map((obj) => ({
+//         ...obj,
+//        owner: "65e8b3fffdc903d86e01648c"
+//     }))
+//     await SampleListing.insertMany(initdata);
+    // const data = Product.find({});
+//     const data=await Post.find({tag:"668ec1d3152270bae825a11d"}).populate('owner');
+console.log(data);
+}
+
+initialize().catch((err) => {
+    console.log(err);
+})
